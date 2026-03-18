021ea1fce9b24b2718a1b1976a0af0fcaa37b725
ejandoubi <abdelkarim.eljandoubi@outlook.fr>
2026-03-18 19:47:02 +0100

    implement sft_microbatch_train_step function and update test to utilize it

--- cs336_alignment/sft.py
+++ cs336_alignment/sft.py
@@ -106,5 +106,29 @@ def masked_normalize(
     Returns:
     torch.Tensor the normalized sum, where masked elements (mask == 0) don’t contribute to
     the sum."""  
-    return (tensor * mask).sum(dim=dim) / normalize_constant
+    return (tensor * mask.to(tensor.dtype)).sum(dim=dim) / normalize_constant
 
+def sft_microbatch_train_step(
+    policy_log_probs: torch.Tensor,
+    response_mask: torch.Tensor,
+    gradient_accumulation_steps: int,
+    normalize_constant: float = 1.0,
+    ) -> tuple[torch.Tensor, dict[str, torch.Tensor]]:
+    """Execute a forward-and-backward pass on a microbatch.
+    Args:
+    policy_log_probs (batch_size, sequence_length), per-token log-probabilities from the
+    SFT policy being trained.
+    response_mask (batch_size, sequence_length), 1 for response tokens, 0 for
+    prompt/padding.
+    gradient_accumulation_steps Number of microbatches per optimizer step.
+    normalize_constant The constant by which to divide the sum. It is fine to leave this as 1.0.
+    Returns:
+    tuple[torch.Tensor, dict[str, torch.Tensor]].
+    loss scalar tensor. The microbatch loss, adjusted for gradient accumulation. We return
+    this so we can log it.
+    metadata Dict with metadata from the underlying loss call, and any other statistics you
+    might want to log."""
+
+    loss = -masked_normalize(tensor=policy_log_probs, mask=response_mask, normalize_constant=normalize_constant*gradient_accumulation_steps, dim=1).mean() 
+    loss.backward()
+    return loss.detach(), {}
